39079c21d53284558300676cd3f5112433a2ad50
erik <erik@test.com>
2023-06-01 07:15:38 +0000

    use theme in layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style.css");
-  eleventyConfig.addPassthroughCopy("./node_modules/startbootstrap-agency/dist/assets");
-  eleventyConfig.addPassthroughCopy("./node_modules/startbootstrap-agency/dist/css");
-  eleventyConfig.addPassthroughCopy("./node_modules/startbootstrap-agency/dist/js");
+  eleventyConfig.addPassthroughCopy({"./node_modules/startbootstrap-agency/dist/assets": "assets"});
+  eleventyConfig.addPassthroughCopy({"./node_modules/startbootstrap-agency/dist/css": "css"});
+  eleventyConfig.addPassthroughCopy({"./node_modules/startbootstrap-agency/dist/js": "js"});
   // Return your Object options:
     return {
       dir: {
---
+++ src/index.md
@@ -0,0 +1,11 @@
+---
+title: Eriks test
+title1: "Välkommen till Syv-Tina AB"
+title2: "Vägledning och karriärrådgivning"
+layout: "layout/mainpage.njk"
+---
+är detta content??
+
+<h1>funkar html? </h1>
+
+<button class="btn btn-primary">funkar bootstrap</button>
