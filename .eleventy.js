f7ab4fd6b8e50218dc0eb31e9ad9e2b06e843a27
erik <erik@test.com>
2023-06-01 18:45:09 +0000

    added decapcms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css");
-    // Return your Object options:
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
+  // Return your Object options:
     return {
       dir: {
         input: "src",
