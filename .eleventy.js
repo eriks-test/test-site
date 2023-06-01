38cb465aa359e32e87bbc963b6782ed3e532e222
erik <erik@test.com>
2023-06-01 06:48:21 +0000

    added theme

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css");
-    // Return your Object options:
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./node_modules/startbootstrap-agency/dist/assets");
+  eleventyConfig.addPassthroughCopy("./node_modules/startbootstrap-agency/dist/css");
+  eleventyConfig.addPassthroughCopy("./node_modules/startbootstrap-agency/dist/js");
+  // Return your Object options:
     return {
       dir: {
         input: "src",
